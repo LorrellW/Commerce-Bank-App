7115ced63a4f74510842dca05777df4d0f859881
LorrellW <112707694+LorrellW@users.noreply.github.com>
2025-04-14 10:31:34 -0500

    Update page.tsx

--- fortune400/src/app/pages/taxes/page.tsx
+++ fortune400/src/app/pages/taxes/page.tsx
@@ -7,6 +7,7 @@ import { useState, useEffect } from "react";
 
 export default function TaxesPage() {
  
+  const[isOpen, setIsOpen] = useState(false);
   const[data, setData] = useState({});
   const[loading, setLoading] = useState(true);
   const[error, setError] = useState(null);
@@ -44,6 +45,35 @@ else{
         <div>api data here.{JSON.stringify(data)}</div>  
         <h1 className="p-6">api test page</h1>
         <p className="p-6">WTesting API's...</p>
+
+        <button
+        onClick={() =>setIsOpen(true)}
+        className="bg-green-600 text-black w-36 h-20 place-content-center">
+          modal Button
+        </button>
+        
+        { isOpen ? (
+  <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-60 z-10">
+    <div className="relative bg-white w-96 h-72 rounded-lg">
+      <button
+        className="absolute top-0 right-0 m-1 bg-primary text-white w-16 h-8 rounded-lg"
+        onClick={() => setIsOpen(false)}
+      >
+        X
+      </button>
+      <div className="flex items-center justify-center text-black text-xl h-full">
+        Modal Content...
+      </div>
+      <button
+        className="absolute bottom-4 left-1/2 transform -translate-x-1/2 bg-primary text-white rounded-lg w-20 h-12"
+        onClick={() => setIsOpen(false)}
+      >
+        CLOSE
+      </button>
+    </div>
+  </div>
+) : null 
+        }
       </div>
     );
   }
